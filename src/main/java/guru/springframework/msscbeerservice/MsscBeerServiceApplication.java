7fc262c60d79e78362374d8ebc98a8ce243fb58b
canattofilipe <canatto.filipe@gmail.com>
2021-03-03 09:11:55 -0300

    bugfix

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,13 +2,11 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class MsscBeerServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MsscBeerServiceApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(MsscBeerServiceApplication.class, args);
+  }
 }
